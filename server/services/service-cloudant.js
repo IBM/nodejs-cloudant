cc308d789f065ba1bb00460933e0378830c3c23f
jkingoliver <king@us.ibm.com>
2019-09-16 11:24:48 -0500

    remove unecessary service files, setup local dev

--- server/services/service-cloudant.js
+++
@@ -1,7 +0,0 @@
-const IBMCloudEnv = require('ibm-cloud-env');
-const CloudantSDK = require('@cloudant/cloudant');
-
-module.exports = function(app, serviceManager){
-	const cloudant = new CloudantSDK(IBMCloudEnv.getString('cloudant_url'));
-	serviceManager.set('cloudant', cloudant);
-};
