852c861faeec541ef545c400e4f048679f299ba7
Chuck Cox <chuckc@us.ibm.com>
2020-02-19 14:24:13 -0600

    chore: add bash execution to experience

--- scripts/experience_test.sh
+++ scripts/experience_test.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+echo "Installing Chrome web driver..."
+./install_chrome.sh
+
+echo "Starting Chrome web driver..."
+./start_chrome.sh
+
 echo "Downloading and installing pip..."
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
