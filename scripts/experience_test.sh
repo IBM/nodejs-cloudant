c81453cb02f2b6c8d340f616cae21db2504ca268
Gabriel Valencia <gvalenc@us.ibm.com>
2020-09-21 11:44:53 -0500

    fix: change to pip3

--- scripts/experience_test.sh
+++ scripts/experience_test.sh
@@ -7,10 +7,10 @@ echo "Starting Chrome web driver..."
 source <(curl -sSL "${DEVX_SKIT_ASSETS_GIT_URL_RAW:-https://github.com/IBM/devex-skit-assets/raw/v1.0.0}/scripts/start_chrome.sh")
 
 echo "Checking for pip"
-pip -V
+pip3 -V
 
 echo "Installing Selenium Python package..."
-pip install selenium
+pip3 install selenium
 
 echo "Running UI test using Selenium..."
 python3 experience_test.py
