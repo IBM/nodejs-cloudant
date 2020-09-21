5b4063b18810056bca9a3f195ee2a2bbf2d7c643
Gabriel Valencia <gvalenc@us.ibm.com>
2020-09-21 11:34:35 -0500

    fix: dont install pip

--- scripts/experience_test.sh
+++ scripts/experience_test.sh
@@ -6,9 +6,7 @@ source <(curl -sSL "${DEVX_SKIT_ASSETS_GIT_URL_RAW:-https://github.com/IBM/devex
 echo "Starting Chrome web driver..."
 source <(curl -sSL "${DEVX_SKIT_ASSETS_GIT_URL_RAW:-https://github.com/IBM/devex-skit-assets/raw/v1.0.0}/scripts/start_chrome.sh")
 
-echo "Downloading and installing pip..."
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py
+echo "Checking for pip"
 pip -V
 
 echo "Installing Selenium Python package..."
