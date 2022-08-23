cc8b9f98099a1ffbe1448a3dd2a37d781efce2a4
marifse <50728232+marifse@users.noreply.github.com>
2022-08-24 02:17:29 +0500

    Update main.tf

--- terraform/simple-kube/main.tf
+++ terraform/simple-kube/main.tf
@@ -1,10 +1,27 @@
 terraform {
   required_version = ">= 0.12"
+  required_providers {
+    ibm = {
+    source = "IBM-Cloud/ibm"
+    version = ">= 1.0.0"
+    }
 }
+}
+
+#provider "ibm" {
+#  ibmcloud_api_key   = "${var.ibmcloud_api_key}"
+#}
+
 
 resource "random_string" "random" {
   length = 4
   min_lower = 4
+}
+
+resource "time_sleep" "wait_600_seconds" {
+  depends_on = [ibm_container_cluster.cluster]
+
+  create_duration = "600s"
 }
 
 data "ibm_resource_group" "group" {
@@ -22,8 +39,8 @@ resource "ibm_container_cluster" "cluster" {
   machine_type      = var.machine_type
   hardware          = var.hardware
   kube_version      = var.kube_version
-  public_vlan_id    = var.public_vlan_num
-  private_vlan_id   = var.private_vlan_num
+  #public_vlan_id    = var.public_vlan_num
+  #private_vlan_id   = var.private_vlan_num
   resource_group_id = data.ibm_resource_group.group.id
 }
 
@@ -36,8 +53,9 @@ resource "ibm_resource_instance" "cloudant" {
 }
 
 resource "ibm_container_bind_service" "bind_service" {
+  depends_on = [time_sleep.wait_600_seconds]
   cluster_name_id       = var.cluster_name
-  service_instance_name   = ibm_resource_instance.cloudant.name
+  service_instance_name = ibm_resource_instance.cloudant.name
   namespace_id          = var.cluster_namespace
   resource_group_id = data.ibm_resource_group.group.id
 }
@@ -49,7 +67,8 @@ module "ibm-kubernetes-toolchain" {
   toolchain_name    = "cloudant-terraform-toolchain-node-${random_string.random.result}"
   application_repo  = "https://github.com/triceam/nodejs-cloudant"
   resource_group    = var.resource_group
-  cluster_name      = ibm_container_cluster.cluster.name
+  cluster_name      = var.cluster_name
   cluster_namespace = var.cluster_namespace
   container_registry_namespace = var.container_registry_namespace
+  depends_on = [ibm_container_cluster.cluster]
 }
